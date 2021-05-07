7e022beee44009cd506d38bdaaeb70b3516da7ef
Alexander Alvarez <67664433+Alex553-bot@users.noreply.github.com>
2021-05-07 10:22:41 -0400

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,19 @@
+public class Ejercicio2{
+
+    public int generarSerie(int n){
+        int num = 0;
+        int numAnt1 = 1;
+        int numAnt2 = 1;
+        int numAnt3 = 1;
+
+        for(int i = 3; i < n; i++){
+            num = numAnt2 + numAnt3;
+
+            numAnt3 = numAnt2;
+            numAnt2 = numAnt1;
+            numAnt1 = num;
+            System.out.println(num);
+        }
+        return num;
+    }
+}
