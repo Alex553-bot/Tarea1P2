19aa7f4092b0cb5616e3ee2106de3d5b46adc6f8
Alexander Alvarez <67664433+Alex553-bot@users.noreply.github.com>
2021-05-07 10:28:38 -0400

    Update Ejercicio2.java

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -5,13 +5,17 @@ public class Ejercicio2{
         int numAnt1 = 1;
         int numAnt2 = 1;
         int numAnt3 = 1;
-
+        System.out.println(numAnt3);
+        System.out.println(numAnt2);
+        System.out.println(numAnt1);
+        
         for(int i = 3; i < n; i++){
             num = numAnt2 + numAnt3;
 
             numAnt3 = numAnt2;
             numAnt2 = numAnt1;
             numAnt1 = num;
+            
             System.out.println(num);
         }
         return num;
